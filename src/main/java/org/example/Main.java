0574e5cc9d6c2420605615d45a428bf2dd060a38
sugpon <suganya.sugu88@gmail.com>
2025-06-17 09:50:56 -0500

    Completed assignment with all todo's

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -27,8 +27,10 @@ public class Main {
      * @return The result of the division
      */
     public static double safeDivision(double a, double b) {
-        // todo: Handle the unchecked ArithmeticException by returning 0.
-        return divide(a, b);
+        if (b == 0) {
+            return 0.0;
+        }
+        return a/b;
     }
 
     /**
@@ -40,10 +42,15 @@ public class Main {
      */
     public static String getSecondLetter(String s, List<String> logMessages) {
         // TODO: Handle the StringIndexOutOfBoundsException by returning an empty string ("")
-        return s.substring(1, 2);
-
+        try {
+            return s.substring(1, 2);
+        } catch (StringIndexOutOfBoundsException e) {
+            return "";
+        }
         // TODO: This line needs to happen after the try/catch block, regardless of whether an exception is thrown (put it in a finally block)
-        logMessages.add("Exiting method");
+        finally {
+            logMessages.add("Exiting method");
+        }
     }
 
     /**
@@ -56,6 +63,9 @@ public class Main {
     public static double safeDivideWithCustomException(double a, double b) throws CustomDivideByZeroException {
         // TODO: handle divide by zero by throwing a CustomDivideByZeroException
         // TODO: Modify the custom exception to pass a,b to the constructor and store them in the exception (see CustomDivideByZeroException.java)
-        return divide(a, b);
+        if (b == 0) {
+            throw new CustomDivideByZeroException(a, b);
+        }
+        return a / b;
     }
 }
